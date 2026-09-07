85b9847de8749b48a4ce75996784d19c1a26df6e
Arvin Najafi <arvinnajafi20122120@gmail.com>
2026-09-07 12:35:15 -0700

    add ai delay

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const rubikaBase = `https://botapi.rubika.ir/v3/${rubikaToken}`;
 let offsetId = null;
 
 async function askQwen(userMessage) {
+await new Promise(resolve => setTimeout(resolve, 3000));
   const response = await axios.post(
     "https://api.b.ai/v1/chat/completions",
     {
